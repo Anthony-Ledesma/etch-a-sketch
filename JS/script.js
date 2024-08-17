87b46cc1a54c58c85a11bed478f1a0fd64f4e41d
Anthony Ledesma <165110055+Anthony-Ledesma@users.noreply.github.com>
2024-08-17 11:47:30 -0700

    Refactor code to allow drawing after size changes
    user can now draw after defining width for new grid although now the
    grid is no longer a square and must be fixed

--- JS/script.js
+++ JS/script.js
@@ -1,23 +1,48 @@
 const container = document.querySelector('#grid-container');
 
-function createGrid() {
-    for (i = 0; i < 16; i++) {
+const button = document.querySelector('#reset-button');
+
+createGrid();
+
+button.addEventListener('click', function confirmWidth() {
+    let answer = prompt('How many squares wide would you like to make the canvas?', '16');
+    if (Number(answer) > 100) {
+        confirmWidth();
+    } else {
+        function deleteGrid() {
+            const getSquares = document.querySelectorAll('.grid-square');
+
+            getSquares.forEach((box) => box.remove());
+            const rows = document.querySelectorAll('[class*="grid-row"]');
+            rows.forEach(row => row.remove)
+            };
+            deleteGrid();
+        createGrid(answer);
+    }
+})
+
+
+function createGrid(width = 16) {
+    for (i = 0; i <= width; i++) {
         const row = document.createElement('div');
         row.setAttribute('class', `grid-row${i}`);
         container.appendChild(row);
-        for (j = 0; j < 16; j++) {
+        for (j = 0; j <= width; j++) {
             const getRow = document.querySelector(`.grid-row${i}`);
             const square = document.createElement('div');
             square.setAttribute('class', 'grid-square');
             getRow.appendChild(square);
         }
     }
+    initiateEventListener();
 };
-createGrid();
 
-const squares = document.querySelectorAll('.grid-square');
-for (let i = 0; i < squares.length; i++) {
-    squares[i].addEventListener('mouseover', () => {
-        squares[i].style.backgroundColor = '#000000';
-    })
-}
+function initiateEventListener() {
+    const squares = document.querySelectorAll('.grid-square');
+
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].addEventListener('mouseover', () => {
+            squares[i].style.backgroundColor = '#000000';
+        });
+    };
+};
