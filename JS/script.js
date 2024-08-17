32ccb1d7105a83fdc5235a5e97c78149702f78d4
Anthony Ledesma <165110055+Anthony-Ledesma@users.noreply.github.com>
2024-08-17 15:32:22 -0700

    Create function to delete old grid
    existing grid now properly deletes before the new grid is created

--- JS/script.js
+++ JS/script.js
@@ -2,32 +2,32 @@ const container = document.querySelector('#grid-container');
 
 const button = document.querySelector('#reset-button');
 
+
 createGrid();
 
-button.addEventListener('click', function confirmWidth() {
+button.addEventListener('click', 
+    function confirmWidth() {
     let answer = prompt('How many squares wide would you like to make the canvas?', '16');
     if (Number(answer) > 100) {
         confirmWidth();
     } else {
         function deleteGrid() {
             const getSquares = document.querySelectorAll('.grid-square');
-
+            const getRows = document.querySelectorAll('[class*="grid-row"]');
             getSquares.forEach((box) => box.remove());
-            const rows = document.querySelectorAll('[class*="grid-row"]');
-            rows.forEach(row => row.remove)
+            getRows.forEach(row => row.remove())
             };
-            deleteGrid();
+        deleteGrid();
         createGrid(answer);
     }
 })
 
-
 function createGrid(width = 16) {
-    for (i = 0; i <= width; i++) {
+    for (i = 0; i < width; i++) {
         const row = document.createElement('div');
         row.setAttribute('class', `grid-row${i}`);
         container.appendChild(row);
-        for (j = 0; j <= width; j++) {
+        for (j = 0; j < width; j++) {
             const getRow = document.querySelector(`.grid-row${i}`);
             const square = document.createElement('div');
             square.setAttribute('class', 'grid-square');
